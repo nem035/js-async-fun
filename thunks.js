0d7e5401adc8949732abce606a96b5bafd5c283d
nem035 <n.stojanovic035@gmail.com>
2016-07-16 07:12:58 -0400

    finished thunks

--- thunks.js
+++ thunks.js
@@ -47,7 +47,7 @@ We can create a function that will
 create thunks for us
 
 @example
-function toThunk(fn, ...args) {
+function makeThunk(fn, ...args) {
   return function(cb) {
     args.push(cb)
     fn(...args);
@@ -58,12 +58,68 @@ function add(x, y, cb) {
   cb(x + y);
 }
 
-const thunk = toThunk(add, 10, 15);
+const thunk = makeThunk(add, 10, 15);
 
 thunk(function(sum) {
   sum; // 25
 });
+
+An active thunk is a thunk that
+precomputes its value when created
+and then returns it on all
+subsequent calls.
+
+A lazy thunk is a thunk that
+computes its value the first
+time it is called, then
+returns it on all subsequent
+calls.
 */
 function thunks() {
 
+  start('thunks');
+
+  const thunkFile1 = getFile('file1');
+  const thunkFile2 = getFile('file2');
+  const thunkFile3 = getFile('file3');
+
+  thunkFile1(contents1 => {
+    renderFile('file1', contents1);
+    thunkFile2(contents2 => {
+      renderFile('file2', contents2);
+      thunkFile3(contents3 => {
+        renderFile('file3', contents3);
+        finish('thunks');
+      });
+    });
+  });
+
+  // A thunk maker that caches the
+  // callback or the response,
+  // depending on which is obtained
+  // first and executes the callback
+  // with the response, once both are
+  // received
+  function getFile(file) {
+    let contents;
+    let callback;
+    fakeAjax('thunks', file, (response) => {
+      if (callback === undefined) {
+        contents = response;
+      } else {
+        callback(response);
+      }
+    });
+    return function(cb) {
+      if (contents === undefined) {
+        callback = cb;
+      } else {
+        cb(contents);
+      }
+    };
+  }
+
+  function renderFile(file, contents) {
+    render('thunks', file, contents);
+  }
 }
